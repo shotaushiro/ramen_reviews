19d71bca421452dcf12b0bb7b7335db79945153f
shota.ushiro <deeper0120@icloud.com>
2020-07-02 14:09:09 +0000

    test

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,6 @@
 class ListsController < ApplicationController
   before_action :set_list, only: [:show, :edit, :update, :destroy]
-
+  before_action :require_user_logged_in
   # GET /lists
   # GET /lists.json
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
   
+  resources :lists
+  
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get 'users/index'
   get 'users/show'
   get 'users/new'
   get 'users/create'
-  resources :lists
-  
+
   root to: 'lists#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
   
